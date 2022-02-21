fe98e6777bb1c3e44f21cdc273dd109c7f284b86
sumomo99 <ishikawaj0@gmail.com>
2022-02-22 07:25:31 +0900

    単体テストを追加

--- X-Power/tests/X-PowerTests/X-PowerTests.cs
+++ X-Power/tests/X-PowerTests/X-PowerTests.cs
@@ -1,12 +1,19 @@
 using Xunit;
+using XPower;
 
 namespace X_PowerTests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(3, 2, 9d)]
+    [InlineData(3, 0, 1d)]
+    [InlineData(0, 3, 0d)]
+    [InlineData(-3, 2, 9d)]
+    [InlineData(-3, 3, -27d)]
+    [InlineData(2, -2, 0.25d)]
+    public void PowerTest(int x, int y, double ans)
     {
-        Assert.Equal(9, XPower.Power.CalculatePower("3", "2"));
+        Assert.Equal(Power.CalculatePower(x, y), ans);
     }
 }
